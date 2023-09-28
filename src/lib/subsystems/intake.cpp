9ee2c402c886eee0ddd5aa8fcd285531a3bf8c51
BattleCh1cken <BattleCh1cken@larkov.de>
2023-09-28 14:15:19 -0400

    Formatted intake and removed unecessary print statements

--- src/lib/subsystems/intake.cpp
+++ src/lib/subsystems/intake.cpp
@@ -11,16 +11,12 @@ namespace lib {
 Intake::Intake(std::shared_ptr<pros::Motor> i_motor) : motor(i_motor){};
 
 void Intake::loop() {
-  while (true) {
-    if (get_state() == IntakeState::Idle) {
-      printf("just chillin\n");
-      motor->move(0);
-    } else if (get_state() == IntakeState::Running) {
-      motor->move(127);
-    } else if (get_state() == IntakeState::Reversed) {
-      motor->move(-127);
-    }
-    pros::delay(20);
+  if (get_state() == IntakeState::Idle) {
+    motor->move(0);
+  } else if (get_state() == IntakeState::Running) {
+    motor->move(127);
+  } else if (get_state() == IntakeState::Reversed) {
+    motor->move(-127);
   }
 };
 }  // namespace lib
