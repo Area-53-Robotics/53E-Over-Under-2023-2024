7853062573b6de46382c775082ada2e2a5af86bf
BattleCh1cken <BattleCh1cken@larkov.de>
2023-09-28 14:14:11 -0400

    Completed catapult code

--- include/lib/subsystems/catapult.hpp
+++ include/lib/subsystems/catapult.hpp
@@ -3,19 +3,24 @@
 
 #include "lib/utils/state_machine.hpp"
 #include "lib/utils/task-wrapper.hpp"
+#include "pros/adi.hpp"
 #include "pros/motors.hpp"
 
 namespace lib {
 
-enum class CatapultState { Idle, Loading, Ready, Firing };
+enum class CatapultState { Idle, Loading, Ready, Firing, Repeating };
 
 class Catapult : public lib::StateMachine<CatapultState>, public TaskWrapper {
  public:
-  Catapult(std::shared_ptr<pros::Motor> i_motor);
-  ~Catapult();
+  Catapult(std::shared_ptr<pros::Motor> i_motor,
+           std::shared_ptr<pros::ADIDigitalIn> i_limit_switch);
+
+  void fire();
+  void toggle_repeating();
 
  private:
   std::shared_ptr<pros::Motor> motor;
+  std::shared_ptr<pros::ADIDigitalIn> limit_switch;
 
  protected:
   void loop() override;
--- src/lib/subsystems/catapult.cpp
+++ src/lib/subsystems/catapult.cpp
@@ -3,17 +3,76 @@
 #include <cstdio>
 
 #include "pros/rtos.hpp"
+#include "pros/screen.hpp"
 
 namespace lib {
 
-Catapult::Catapult(std::shared_ptr<pros::Motor> i_motor) : motor(i_motor){};
-Catapult::~Catapult(){};
-void Catapult::loop() {
-  while (true) {
-    if (get_state() == CatapultState::Idle) {
-      printf("just chillin\n");
-    }
-    pros::delay(20);
+Catapult::Catapult(std::shared_ptr<pros::Motor> i_motor,
+                   std::shared_ptr<pros::ADIDigitalIn> i_limit_switch)
+    : motor(i_motor), limit_switch(i_limit_switch) {
+  printf("setting initial mode to loading");
+  motor->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+
+  set_state(CatapultState::Loading);
+};
+
+void Catapult::fire() {
+  if (get_state() == CatapultState::Ready) {
+    printf("firing\n");
+    set_state(CatapultState::Firing);
   }
+}
+
+void Catapult::toggle_repeating() {
+  if (!(get_state() == CatapultState::Repeating)) {
+    set_state(CatapultState::Repeating);
+  } else {
+    set_state(CatapultState::Loading);
+  }
+}
+
+void Catapult::loop() {
+  switch (get_state()) {
+    case CatapultState::Idle: {
+      printf("idle\n");
+      // This mode is meant to to completely disable the catapult
+      motor->move(0);
+      break;
+    }
+
+    case CatapultState::Loading: {
+      if (limit_switch->get_value()) {
+        set_state(CatapultState::Ready);
+      }
+
+      motor->move(100);
+      break;
+    }
+
+    case CatapultState::Ready: {
+      printf("ready\n");
+      if (!limit_switch->get_value()) {
+        set_state(CatapultState::Loading);
+      }
+
+      motor->move(0);
+      break;
+    }
+
+    case CatapultState::Firing: {
+      printf("Firing\n");
+      while (limit_switch->get_value()) {
+        motor->move(127);
+      }
+      set_state(CatapultState::Loading);
+      break;
+    }
+
+    case CatapultState::Repeating: {
+      // motor->move(127);
+      break;
+    }
+  }
+  pros::delay(20);
 };
 }  // namespace lib
